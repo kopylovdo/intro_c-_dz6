bd20d06d1b7c1a3f168e80cfad93c77d2a651c86
Дмитрий Копылов <dkopylov@MacBook-Air-2.local>
2022-09-07 19:44:23 +0300

    Tsak1 init commit

---
+++ programm_001_count_nums/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Программа, которая считате сколько чисел больше 0 ввёл пользователь. Программа запрашивает натуральное число M, пользователь вводит с клавиатуры M чисел. 
+
+Console.Write("Введите количество чисел M ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+
+int count_nums_above_zero = 0;
+
+for (int index = 0; index < M; index++)
+{
+    Console.Write("Введите число ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0)
+    {
+        count_nums_above_zero++;
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine($"Количество чисел больше 0 - {count_nums_above_zero} ");
